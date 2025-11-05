d468dbef92133fa1aa9929dabd6f7d723c93d828
jersonCont <71796410@continental.edu.pe>
2025-11-05 12:41:27 -0500

    pequeño cambio de prueba

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def saludo(nombre: str) -> str:
-    return f"hola, {nombre}! bienvenido a git, commit exitoso :)."
+    return f"hola, {nombre}! bienvenido a git, commit exitoso."
 
 if __name__ == "__main__":
     nombre = input("¿tu nombre? ")
     print(saludo(nombre))
-    
+    print("editar desde github")
